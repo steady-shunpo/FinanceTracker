df89dc42b94d5e4052b85d6809ba961b06f39d15
steady-shunpo <shunpoguy@gmail.com>
2025-06-18 10:56:37 +0530

    changed python executable

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dbgr from 'debug'
 const dbg=dbgr("development:Mongoose");
 
-const mongo=mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ojpowz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+const mongo=mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ojpowz.mongodb.net/paymentApp?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     dbg("connected")
 }).catch((err)=>{
